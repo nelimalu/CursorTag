87ecb79a1fc258275ab2b19e2c985723e0d04bb9
Luka Jovanovic <lukaj2501@gmail.com>
2021-02-02 20:21:00 -0500

    Added server support

--- index.js
+++ index.js
@@ -1,3 +1,45 @@
+var socket = new WebSocket("ws://99.241.147.138:1345");
+
+const colour = "rgba(" + randint(0, 255).toString() + "," + randint(0, 255).toString() + "," + randint(0, 255).toString() + ",1)"
+var users = [];
+
+var mouse = {
+	x: canvas.width / 2,
+	y: canvas.height / 2
+}
+
+function User(colour, x, y) {
+	this.colour = colour;
+}
+
+var localUser = User(colour, mouse.x, mouse.y);
+
+socket.onopen = function(event) {
+	console.log("[CONNECTION] Connection established.");
+
+	socket.send("JOIN" + JSON.stringify(localUser));
+};
+
+socket.onmessage = function(event) {
+	console.log("[INFO] Recieved data from server: " + event.data)
+};
+
+socket.onclose = function(event) {
+	console.log('[CLOSE] Connection died');
+};
+
+socket.onerror = function(error) {
+	alert('[ERROR] Could not connect to the server. Please try again later.');
+};
+
+
+/*
+<=====<!>====>
+ CANVAS STUFF
+<=====<!>====>
+*/
+
+
 var canvas = document.querySelector('canvas');
 
 canvas.width = 800;
@@ -5,17 +47,10 @@ canvas.height = 700;
 
 var c = canvas.getContext('2d');
 
-const colour = "rgba(" + randint(0, 255).toString() + "," + randint(0, 255).toString() + "," + randint(0, 255).toString() + ",1)"
-
 canvas.requestPointerLock = canvas.requestPointerLock || canvas.mozRequestPointerLock;
 canvas.exitPointerLock = canvas.exitPointerLock || canvas.mozExitPointerLock;
 
 var MOUSE_LOCKED = false;
-
-var mouse = {
-	x: canvas.width / 2,
-	y: canvas.height / 2
-}
 
 function randint(min, max) {
 	return Math.floor(Math.random() * Math.floor(max - min)) + min;
